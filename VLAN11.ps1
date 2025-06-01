91697449b37c96e01b08ed6b670b6abb576f6482
AKArce94 <akarce94@gmail.com>
2025-06-01 16:45:24 +0800

    Add files via upload

---
+++ VLAN11.ps1
@@ -0,0 +1,27 @@
+﻿D1: vlan creation + dhcp+svi:
+vlan 11
+ name vlan11.com.ph
+ exit
+Int vlan 11
+ no shut
+ ip add 10.0.0.65 255.255.255.192
+ ip ospf 1 area 0
+ip dhcp excluded-add 10.0.0.65 10.0.0.70
+ip dhcp pool vlan11.com.ph
+ network 10.0.0.64 255.255.255.192
+ default-router 10.0.0.65
+ domain-name vlan11.com.ph
+
+ @A1:
+ config t
+
+ Int e0/0
+  switchport mode access
+  switchport access vlan 11
+  do sh ip dhcp binding
+  
+ @p1
+config t
+Int e0/0
+no shut
+ip address dhcp
---
+++ VLAN12.ps1
@@ -0,0 +1,27 @@
+﻿D2: vlan creation + dhcp+svi:
+vlan 12
+ name vlan12.com.ph
+ exit
+Int vlan 12
+ no shut
+ ip add 10.0.0.129 255.255.255.128
+ ip ospf 1 area 0
+ip dhcp excluded-add 10.0.0.129 10.0.0.139
+ip dhcp pool vlan12.com.ph
+ network 10.0.0.129 255.255.255.128
+ default-router 10.0.0.128
+ domain-name vlan12.com.ph
+
+ @A1:
+ config t
+
+ Int e1/0
+  switchport mode access
+  switchport access vlan 12
+  do sh ip dhcp binding
+  
+ @p1
+config t
+Int e1/0
+no shut
+ip address dhcp
